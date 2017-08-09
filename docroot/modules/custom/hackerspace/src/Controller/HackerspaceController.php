bed995a5015ac169578beabda155c02d19b65ef8
Alex Sadleir <maxious@lambdacomplex.org>
2017-08-09 20:38:23 +1000

    add project export

--- docroot/modules/custom/hackerspace/src/Controller/HackerspaceController.php
+++ docroot/modules/custom/hackerspace/src/Controller/HackerspaceController.php
@@ -42,7 +42,7 @@ class HackerspaceController extends ControllerBase
 
     foreach (\Drupal::entityManager()->getStorage('paragraph')->loadMultiple($query->execute()) as $tm) {
       $tms[] = [
-        "team_id" => $tm->parent_id->value,
+        "project_id" => $tm->parent_id->value,
         "project_name" => $tm->getParentEntity()->title->value,
         "team_name" => $tm->getParentEntity()->field_team_name->value,
         "name" => $tm->field_name[0]->value,
@@ -54,6 +54,48 @@ class HackerspaceController extends ControllerBase
     $response->setContent(\Drupal::service('serializer')->serialize($tms, 'csv'));
     $response->headers->set('Content-Type', 'text/csv');
     $response->headers->set('Content-Disposition', 'attachment; filename=team_export.' . date("Ymd\THis") . '.csv');
+    return $response;
+  }
+  public function project_export()
+  {
+    $jurisdictions = [];
+    foreach(\Drupal::entityManager()->getStorage('taxonomy_term')->loadTree('jurisdiction') as $term) {
+      $jurisdictions[$term->tid] = $term->name;
+    }
+    $event_locations = [];
+    foreach(\Drupal::entityManager()->getStorage('taxonomy_term')->loadTree('event_location') as $term) {
+      $event_locations[$term->tid] = $term->name;
+    }
+    $prizes = [];
+    foreach(\Drupal::entityManager()->getStorage('taxonomy_term')->loadTree('prize') as $term) {
+      $prizes[$term->tid] = $term->name;
+    }
+
+    $response = new Response();
+    $query = \Drupal::entityQuery('node');
+    $query->condition('type', 'project');
+    $tms = [];
+
+    foreach (\Drupal::entityManager()->getStorage('node')->loadMultiple($query->execute()) as $tm) {
+      foreach ($tm->field_prizes as $prize) {
+        $tms[] = [
+          "project_id" => $tm->nid->value,
+          "team_name" => $tm->field_team_name->value,
+          "project_name" => $tm->title->value,
+          "description" => $tm->body->value,
+          "further_opportunities" => $tm->field_further_opportunities->value,
+          "jurisdiction" => $jurisdictions[$tm->field_jurisdiction[0]->target_id],
+          "event_location" => $event_locations[$tm->field_location[0]->target_id],
+          "jurisdiction_id" => $tm->field_jurisdiction[0]->target_id,
+          "event_location_id" => $tm->field_location[0]->target_id,
+          "prize_id" => $prize->target_id,
+          "prize" => $prizes[$prize->target_id]
+        ];
+      }
+    }
+    $response->setContent(\Drupal::service('serializer')->serialize($tms, 'csv'));
+    $response->headers->set('Content-Type', 'text/csv');
+    $response->headers->set('Content-Disposition', 'attachment; filename=project_export.' . date("Ymd\THis") . '.csv');
     return $response;
   }
 }
